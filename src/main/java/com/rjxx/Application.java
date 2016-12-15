c5d7bc427116421ba78c3076404304ab258dc4c2
张兵 <zhangbing@datarj.com>
2016-12-15 18:06:17 +0800

    修改项目

--- src/main/java/com/rjxx/Application.java
+++ src/main/java/com/rjxx/Application.java
@@ -1,7 +1,5 @@
 package com.rjxx;
 
-import com.rjxx.comm.mybatis.MybatisRepository;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
@@ -11,7 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * Created by admin on 2016/1/6.
  */
 @SpringBootApplication
-@MapperScan(annotationClass = MybatisRepository.class, basePackages = {"com.rjxx"})
 @EnableScheduling
 public class Application extends SpringBootServletInitializer {
 
