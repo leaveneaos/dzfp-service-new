bc5c57841640c0bf83e117e861c71f1c72762623
zhangsongqiang <zhangsongqiang@datarj.com>
2018-08-01 17:57:07 +0800

    添加跨越注解

--- src/main/java/com/rjxx/taxeasey/controller/common/AdapterPostController.java
+++ src/main/java/com/rjxx/taxeasey/controller/common/AdapterPostController.java
@@ -13,10 +13,7 @@ import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -29,6 +26,7 @@ import java.util.LinkedHashMap;
  */
 @RequestMapping("/kptService")
 @RestController
+@CrossOrigin
 public class AdapterPostController {
     @Autowired
     private GsxxJpaDao gsxxJpaDao;
